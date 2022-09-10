171d4315352d2f3ef51a4f8543645dccff5616c5
fherrerao <fherrera0206@gmail.com>
2022-09-09 21:10:38 -0500

    Add routes to the site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import TodoContainer from "./components/TodoContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<TodoContainer />} />      
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
