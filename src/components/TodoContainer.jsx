77b995d3f49059bfa44285c60b59fea60f03aa05
fherrerao <fherrera0206@gmail.com>
2022-09-11 16:02:59 -0500

    Adding todos array within the useEffect hook

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -2,32 +2,41 @@ import { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 
 const TodoContainer = () => {
-  const todos = [
-    {
-      id: 1,
-      title: "Setup development environment",
-      completed: true
-    },
-    {
-      id: 2,
-      title: "Develop website and add content",
-      completed: false
-    },
-    {
-      id: 3,
-      title: "Deploy to live server",
-      completed: false
-    }
-  ];
-
   const [data, setData] = useState([]);
-  useEffect(() => {  
+
+  useEffect(() => {
+    const todos = [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ];
     setData(todos);
   },[]);
+
+  const handleChange = (id) => {
+    data.forEach(item => {
+      if(item.id === id){
+        item.completed = !item.completed;
+        setData([...data]);
+      }
+    });
+  }
   
   return(
     <div>
-      <TodoList todos={data} />
+      <TodoList todos={data} handleChangeProps = {handleChange}/>
     </div>
   );
 }
