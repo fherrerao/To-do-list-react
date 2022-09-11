e27cd0a0ce8713612cd62c694ea2aec85aa260b0
fherrerao <fherrera0206@gmail.com>
2022-09-11 16:02:11 -0500

    Adding Header for all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import TodoContainer from "./components/TodoContainer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <HashRouter basename="/">
+        <Header />
         <Routes>
           <Route path="/" element={<TodoContainer />} />      
         </Routes>
