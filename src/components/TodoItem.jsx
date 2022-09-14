d6c58417b56efdc4f7bf0071fbab72ad2be07a67
fherrerao <fherrera0206@gmail.com>
2022-09-14 08:23:12 -0500

    Add input to edit the todo in TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,31 +1,68 @@
-import styles from "./TodoItem.module.css"
+import { useState } from "react";
+import styles from "./TodoItem.module.css";
 
 const TodoItem = (props) => {
+
+  const [edit, setEdit] = useState(false);
+
+  let viewMode = {};
+  let editMode = {};
+
   const completedStyle = {
     fontStyle: "italic",
     color: "#595959",
     opacity: 0.4,
     textDecoration: "line-through",
+  };
+
+  const handleEditing = () => {
+    setEdit(true);
+  };
+
+  edit ? viewMode.display = "none" : editMode.display = "none"
+
+  const handleUpdateDone = (e) => {
+    if(e.key === "Enter"){
+      setEdit(false);
+    }
   }
 
   return (
     <div>
-      {props.todos.todos.map(item => (
-        <li key={item.id} className={styles.item}> 
+      {props.todos.todos.map((item) => (
+        <li key={item.id} className={styles.item}>
+          <div onDoubleClick={handleEditing} style={viewMode}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => props.todos.handleChangeProps(item.id)}
+            />
+            <button
+              onClick={() => {
+                props.deleteItemProp(item.id);
+              }}
+            >
+              Delete
+            </button>
+            <span style={item.completed ? completedStyle : null}>
+              {item.title}
+            </span>
+          </div>
           <input
-            className={styles.checkbox}
-            type="checkbox" 
-            checked={item.completed}
-            onChange={() => props.todos.handleChangeProps(item.id)}
+            type="text" 
+            className={styles.textInput} 
+            style={editMode} 
+            value={item.title}
+            onChange={e => {
+              props.updateItemProp(e.target.value, item.id)
+            }}
+            onKeyDown={handleUpdateDone}
           />
-          <button onClick={()=> {props.deleteItemProp(item.id)}}>Delete</button>
-          <span style={item.completed ? completedStyle : null}>
-            {item.title}
-          </span>
         </li>
       ))}
     </div>
   );
-}
+};
 
 export default TodoItem;
