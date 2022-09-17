01204fffa68f83cf03142a4ef94e4272aa406c2b
fherrerao <fherrera0206@gmail.com>
2022-09-17 12:42:08 -0500

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Router basename="/">
-        <Navbar/>
+        <Navbar />
         <Header />
         <Routes>
           <Route path="/" element={<TodoContainer />} />
